cdcf2960e2df6b1fdbe639ff6314c4c9084997e6
iamvivekchadhuvula <iamvivekchadhuvula@gmail.com>
2024-06-13 21:15:42 +0530

    Skills section is completed

--- vivek/src/App.jsx
+++ vivek/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
+import Skills from './sections/Skills.jsx'
 
 const App = () => {
   return (
-    <div className='w-full flex justify-center items-center h-full bg-slate-800'>
+    <div className='w-full flex flex-col h-full bg-slate-800'>
       <Hero />
+      <Skills />
     </div>
   )
 }
--- vivek/src/sections/Hero.jsx
+++ vivek/src/sections/Hero.jsx
@@ -4,11 +4,12 @@ import Resume from '../assets/VivekChadhuvula_resume.pdf';
 
 const Hero = () => {
     return (
-        <section className='mt-10 flex mb-8 gap-10 flex-wrap items-center justify-center  '>
+        <>
+        <section className='mt-20 flex mb-8 gap-10 flex-wrap items-center justify-center  '>
             <h1 className='text-6xl font-bold text-white '>Vivek <br />
                 Chadhuvula</h1>
             <img src={vivek} alt="Main_Photo" className='rounded-full w-[20rem] float-none' />
-            <p className='text-white flex mr-8 ml-8 mb-8 mt-0'> Welcome!
+            <p className='text-white flex mr-10 ml-10 mb-8 mt-0 text-xl md:text-2xl md:mr-20 md:ml-20 '> Welcome!
                 I'm a passionate web developer with a strong foundation in MERN stack development (MongoDB, Express.js, React.js, and Node.js). I'm excited to showcase my skills and experience on this website, and I'm always looking for new opportunities to collaborate on creative and challenging projects.
             </p>
             <a
@@ -18,7 +19,11 @@ const Hero = () => {
                 target='_blank'
                 rel='noreferrer'
             ><button className='shadow-lg '>Resume</button></a>
-       </section>
+        
+        
+        </section>
+            <div className='h-[0.5px] opacity-10  w-full bg-white'></div>
+        </>
     )
 }
 
