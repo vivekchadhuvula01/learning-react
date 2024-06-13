c2f86d7f718cae97af77fabf2dc1e4f202508c6f
iamvivekchadhuvula <iamvivekchadhuvula@gmail.com>
2024-06-13 18:43:11 +0530

    project Setup and Hero Sections is completed

---
+++ vivek/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './sections/Hero.jsx'
+
+const App = () => {
+  return (
+    <div className='w-full flex justify-center items-center h-full bg-slate-800'>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
